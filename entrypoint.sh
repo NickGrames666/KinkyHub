d6c2fa388634c3bf79f6def253c802c709997328
NickGrames666 <tomwesker666@gmail.com>
2025-08-10 18:49:33 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+PORT=${PORT:-8080}
+
+Запускаем бота в фоне
+python bot.py &
+BOT_PID=$!
+
+Простенький Health-Check HTTP-сервер на порту 8080
+python -m http.server "$PORT" --bind 0.0.0.0 &
+HTTP_PID=$!
+
+wait -n $BOT_PID $HTTP_PID
